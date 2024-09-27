ba5fe6185502b749af255851464ea763914d9602
fernanda iaczinski <110407245+fernandaiaczinski@users.noreply.github.com>
2024-09-27 11:22:06 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,18 +1,27 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-async function visualizarInformacoesGlobais() {
+
+async function vizualizarInformacoesGlobais()
+    {
+        "total_pessoas_conectadas", 5.04e9,
+        "tempo_medio", 2.38,
+        "total_pessoas_mundo", 7.888e9
+      }
+
+ {
     const res = await fetch(url)
     const dados = await res.json()
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    console.log(dados);
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+
     const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')           
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_conectadas}</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
-// código omitido
-    const container = document.getElementById("graficos-container")
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+
+    const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
-    console.log(paragrafo)
 }
-visualizarInformacoesGlobais()
+
+vizualizarInformacoesGlobais()
